dc151782be293ececede6f4e15b65a838a959e9a
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-01 20:13:42 -0300

    added postedDateTime and post actor

--- src/main/java/br/edu/ifpb/entity/Topic.java
+++ src/main/java/br/edu/ifpb/entity/Topic.java
@@ -1,9 +1,11 @@
 package br.edu.ifpb.entity;
 
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  * Created by susanneferraz on 31/08/16.
@@ -17,6 +19,9 @@ public class Topic {
     private String name;
     private String photoPath;
     private String description;
+    private LocalDateTime postedDateTime;
+    @ManyToOne
+    private UserProfile actor;
 
     public Topic() {
     }
@@ -44,6 +49,14 @@ public class Topic {
     public void setPhotoPath(String photoPath) {
         this.photoPath = photoPath;
     }
+    
+    public LocalDateTime getPostedDateTime() {
+        return this.postedDateTime;
+    }
+    
+    public void setPostedDateTime(LocalDateTime postedDateTime) {
+        this.postedDateTime = postedDateTime;
+    }
 
     public String getDescription() {
         return description;
@@ -51,6 +64,14 @@ public class Topic {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+    
+    public UserProfile getActor() {
+        return this.actor;
+    }
+    
+    public void setActor(UserProfile actor) {
+        this.actor = actor;
     }
 
     @Override
@@ -63,6 +84,7 @@ public class Topic {
         if (id != null ? !id.equals(topic.id) : topic.id != null) return false;
         if (name != null ? !name.equals(topic.name) : topic.name != null) return false;
         if (photoPath != null ? !photoPath.equals(topic.photoPath) : topic.photoPath != null) return false;
+        if (postedDateTime != null ? !postedDateTime.equals(topic.postedDateTime) : topic.postedDateTime != null) return false;
         return description != null ? description.equals(topic.description) : topic.description == null;
 
     }
@@ -73,6 +95,7 @@ public class Topic {
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (photoPath != null ? photoPath.hashCode() : 0);
         result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (postedDateTime != null ? postedDateTime.hashCode() : 0);
         return result;
     }
 }
