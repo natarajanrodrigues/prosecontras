100a6f41de1e5ebdfbb6cedf2dc94f8f12ced4ae
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:17:33 -0300

    created a generic JsonObject interface which contains methods to convert Object into Json and Json into Object

--- src/main/java/br/edu/ifpb/Fachada/Facade.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.Fachada;
-
-/**
- * Created by kieckegard on 02/09/2016.
- */
-public class Facade {
-}
--- src/main/java/br/edu/ifpb/entity/JsonObject.java
+++ src/main/java/br/edu/ifpb/entity/JsonObject.java
@@ -3,5 +3,9 @@ package br.edu.ifpb.entity;
 /**
  * Created by kieckegard on 02/09/2016.
  */
-public interface JsonObject {
+public interface JsonObject<T> {
+
+    String getJsonString();
+    T fromJsonString(String json);
+
 }
