d99dcd511b1e90e15e9fbc060994f296a625e593
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:34:36 -0300

    created a service class called OpinionService which contains methods to persist an opinion, save an opinion on cache, get cached opinion, remove cached opinion, and list opinions by topic sorted by date

--- src/main/java/br/edu/ifpb/services/OpinionService.java
+++ src/main/java/br/edu/ifpb/services/OpinionService.java
@@ -1,7 +1,44 @@
 package br.edu.ifpb.services;
 
+import br.edu.ifpb.entity.Opinion;
+import br.edu.ifpb.entity.Topic;
+import br.edu.ifpb.repository.OpinionCacheRepository;
+import br.edu.ifpb.repository.OpinionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
 public class OpinionService {
+
+    @Autowired
+    private OpinionRepository opinionRepository;
+    @Autowired
+    private OpinionCacheRepository opinionCacheRepository;
+
+    public void saveOpinionOnCache(Opinion opinion) {
+        opinionCacheRepository.save(opinion);
+    }
+
+    public void removeCachedOpinion(Opinion opinion) {
+        opinionCacheRepository.remove(opinion.getUser().getEmail());
+    }
+
+    public void getCachedOpinion(String userEmail) {
+        opinionCacheRepository.getCachedOpinion(userEmail);
+    }
+
+    public void publishOpinion(Opinion opinion) {
+        opinionRepository.save(opinion);
+    }
+
+    public List<Opinion> getOpinionsByTopicOrderedByDate(Topic topic) {
+        List<Opinion> opinions = opinionRepository.listByTopic(topic);
+        Collections.sort(opinions);
+        return opinions;
+    }
+
 }
--- src/main/java/br/edu/ifpb/services/TopicService.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.services;
-
-/**
- * Created by kieckegard on 03/09/2016.
- */
-public class TopicService {
-}
