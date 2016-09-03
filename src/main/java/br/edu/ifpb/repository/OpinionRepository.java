9f2810b37304b2f6faf9abfd551d114062693154
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:26:44 -0300

    created an interface that will be responsable to persist and list opinions by topic

--- src/main/java/br/edu/ifpb/repository/OpinionRepository.java
+++ src/main/java/br/edu/ifpb/repository/OpinionRepository.java
@@ -1,7 +1,16 @@
 package br.edu.ifpb.repository;
 
+import br.edu.ifpb.entity.Opinion;
+import br.edu.ifpb.entity.Topic;
+
+import java.util.List;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
 public interface OpinionRepository {
+
+    void save(Opinion opinion);
+    List<Opinion> listByTopic(Topic topic);
+
 }
