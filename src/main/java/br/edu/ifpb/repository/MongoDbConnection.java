e6a270c21e4c39b1a673985a12e10ba501481b1f
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:23:38 -0300

    created an singleton which provides MondoDb connection. MongoDb java driver provides a connection pool too.

--- src/main/java/br/edu/ifpb/repository/MongoDbConnection.java
+++ src/main/java/br/edu/ifpb/repository/MongoDbConnection.java
@@ -1,7 +1,19 @@
 package br.edu.ifpb.repository;
 
+import com.mongodb.MongoClient;
+
 /**
  * Created by kieckegard on 03/09/2016.
  */
 public class MongoDbConnection {
+
+    private static MongoClient mongoClient;
+    private static String host = "localhost";
+
+    public static MongoClient getMongoClient() {
+        if(mongoClient == null)
+            mongoClient = new MongoClient(host);
+        return mongoClient;
+    }
+
 }
