bbd186dc3d0ee766ca4dedda1ac19322ca6d8f96
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:24:54 -0300

    created an repository which will be responsable to save Opinion, remove and get cached opinion in Redis

--- src/main/java/br/edu/ifpb/repository/OpinionCacheRepository.java
+++ src/main/java/br/edu/ifpb/repository/OpinionCacheRepository.java
@@ -1,7 +1,16 @@
 package br.edu.ifpb.repository;
 
+import br.edu.ifpb.entity.Opinion;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
 public interface OpinionCacheRepository {
+
+    void save(Opinion opinion);
+
+    void remove(String userEmail);
+
+    String getCachedOpinion(String userEmail);
+
 }
