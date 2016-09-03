fdf05dbbf317e530317bfef40aa24a5449f2c997
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:30:44 -0300

    created a JsonSerializer to serialize LocalDateTime (JSR-310) to a JsonElement

--- src/main/java/br/edu/ifpb/utils/LocalDateTimeSerializer.java
+++ src/main/java/br/edu/ifpb/utils/LocalDateTimeSerializer.java
@@ -1,7 +1,27 @@
 package br.edu.ifpb.utils;
 
+
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
-public class LocalDateTimeSerializer {
+public class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
+
+    @Override
+    public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
+        long sec = localDateTime.toInstant(ZoneOffset.UTC).getEpochSecond();
+        return new JsonPrimitive(sec);
+    }
 }
