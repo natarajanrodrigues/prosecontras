6c2162576f1a5ca2e69f65a253575687ea4d96fb
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:25:34 -0300

    Redis implemention to OpinionCacheRepository

--- src/main/java/br/edu/ifpb/repository/OpinionCacheRepositoryRedisImpl.java
+++ src/main/java/br/edu/ifpb/repository/OpinionCacheRepositoryRedisImpl.java
@@ -1,7 +1,33 @@
 package br.edu.ifpb.repository;
 
+import br.edu.ifpb.entity.Opinion;
+import redis.clients.jedis.Jedis;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
-public class OpinionCacheRepositoryRedisImpl {
+public class OpinionCacheRepositoryRedisImpl implements OpinionCacheRepository {
+
+    @Override
+    public void save(Opinion opinion) {
+        try (Jedis jedis = JedisConnection.getJedisConnection()) {
+            jedis.set(opinion.getUser().getEmail(), opinion.getJsonString());
+
+            jedis.save();
+        }
+    }
+
+    @Override
+    public void remove(String userEmail) {
+        try (Jedis jedis = JedisConnection.getJedisConnection()) {
+            jedis.del(userEmail);
+        }
+    }
+
+    @Override
+    public String getCachedOpinion(String userEmail) {
+        try (Jedis jedis = JedisConnection.getJedisConnection()) {
+            return jedis.get(userEmail);
+        }
+    }
 }
