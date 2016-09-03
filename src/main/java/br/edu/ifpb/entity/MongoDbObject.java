03914926529e2ca3579087a1eccf26333d90bcf3
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:18:09 -0300

    created an generic MongoDbObject interface which contains methods to convert an object into document and a document into object

--- src/main/java/br/edu/ifpb/entity/MongoDbObject.java
+++ src/main/java/br/edu/ifpb/entity/MongoDbObject.java
@@ -1,7 +1,14 @@
 package br.edu.ifpb.entity;
 
+import org.bson.Document;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
-public interface MongoDbObject {
+public interface MongoDbObject<T> {
+
+    Document toDocument();
+
+    T fromDocument(Document document);
+
 }
