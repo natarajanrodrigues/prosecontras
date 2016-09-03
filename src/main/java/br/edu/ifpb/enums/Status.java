55de222072bd0e0ce9d5e7ca129e4ed7adeaa2f9
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:21:43 -0300

    created an Status enum which represents a relationship between User and Topic On neo4j

--- src/main/java/br/edu/ifpb/enums/Status.java
+++ src/main/java/br/edu/ifpb/enums/Status.java
@@ -1,7 +1,10 @@
 package br.edu.ifpb.enums;
 
+import org.neo4j.graphdb.RelationshipType;
+
 /**
  * Created by kieckegard on 01/09/2016.
  */
-public class Status {
+public enum Status implements RelationshipType{
+    FOR,AGAINST;
 }
