d7ca9a0274a0610bfe8105af902f7e3eadbc33cf
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-03 14:23:03 -0300

    created an singleton which provides Redis connection. Observes that the Jedis API provides a connection pool. So is not bad to have an singleton providing connections to Redis

--- src/main/java/br/edu/ifpb/repository/JedisConnection.java
+++ src/main/java/br/edu/ifpb/repository/JedisConnection.java
@@ -1,7 +1,18 @@
 package br.edu.ifpb.repository;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 /**
  * Created by kieckegard on 02/09/2016.
  */
 public class JedisConnection {
+
+    private static JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+
+    public static Jedis getJedisConnection() {
+        return pool.getResource();
+    }
+
 }
