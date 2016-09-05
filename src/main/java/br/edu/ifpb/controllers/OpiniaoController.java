44ed8017ab5e0b8444c39c66e84608036d808e61
Pedro Arthur <pfernandesvasconcelos@gmail.com>
2016-09-04 23:44:20 -0300

    modified test class

--- src/main/java/br/edu/ifpb/controllers/OpiniaoController.java
+++ src/main/java/br/edu/ifpb/controllers/OpiniaoController.java
@@ -72,7 +72,6 @@ public class OpiniaoController {
         Topic topic = topicoService.findById(idTopico);
 
         Opinion opiniao = (Opinion) httpSession.getAttribute("opinion");
-
         if (!opiniao.hasTopic(topic)) {
             Positioning newPositioning = new Positioning();
 
